c8f5555acf1786c126e0c51ff92b6b4c706927ba
José David Rodriguez Payo <josedavidrp12@gmail.com>
2024-04-14 17:05:41 +0200

    P3

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
+    },
+    resolve: {
+          alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+          }
+        },
+    plugins: [vue()],
   }
 })
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
