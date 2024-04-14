05906c265e0dad9eca58d8cd0f9d6b9808b3639e
José David Rodriguez Payo <josedavidrp12@gmail.com>
2024-04-14 18:03:32 +0200

    test for update prompt

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>A site for events to better the world.</h1>
+    <h2>Made by David Rodriguez, following VueMastery courses</h2>
+    <h3>PWA thanks to vitejs PWA</h3>
   </div>
 </template>
