9565ebd0ff861712de0071b20cb1056e63cfd181
José David Rodriguez Payo <josedavidrp12@gmail.com>
2024-04-14 17:25:22 +0200

    vitepwa

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Final Countdown',
+        start_url: '/',
+        short_name: 'Final Countdown',
+        description: 'Awesome countdown App',
+        theme_color: '#031c36',
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
