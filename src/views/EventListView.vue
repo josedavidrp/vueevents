27cf558abd8cb756e982e428f6b3a5e769e7020f
José David Rodriguez Payo <josedavidrp12@gmail.com>
2024-05-26 19:50:51 +0200

    Pagination

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 const props = defineProps(['page'])
@@ -7,13 +7,16 @@ const events = ref(null)
 
 const page = computed(() => props.page)
 onMounted(() => {
-  EventService.getEvents(2, page.value)
+  watchEffect(() => {
+    events.value = null
+    EventService.getEvents(2, page.value)
     .then((response) => {
       events.value = response.data
     })
     .catch((error) => {
       console.log(error)
     })
+  })
 })
 </script>
 
@@ -21,6 +24,17 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <div class="events" v-if="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    
+    <router-link
+      :to="{name: 'EventList', query: {page: page - 1}}"
+      rel="prev"
+      v-if="page != 1"
+      >Prev page</router-link>
+
+      <router-link
+      :to="{name: 'EventList', query: {page: page + 1}}"
+      rel="next"
+      >Next page</router-link>
   </div>
   <div class="events" v-else>
     <p>No events available or no internet connection</p>
