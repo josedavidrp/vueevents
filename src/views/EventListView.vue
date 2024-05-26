ef71b1a633abf01ae3130173a1dbdaac53cf35a2
José David Rodriguez Payo <josedavidrp12@gmail.com>
2024-05-26 19:56:21 +0200

    pagination2

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -4,14 +4,21 @@ import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 const props = defineProps(['page'])
 const events = ref(null)
+const totalEvents = ref(0)
 
 const page = computed(() => props.page)
+
+const hasNextPage = computed (() => {
+  const totalPages = Math.ceil(totalEvents.value / 2)
+  return page.value < totalPages
+})
 onMounted(() => {
   watchEffect(() => {
     events.value = null
     EventService.getEvents(2, page.value)
     .then((response) => {
-      events.value = response.data
+      events.value = response.data;
+      totalEvents.value = response.headers['x-total-count']
     })
     .catch((error) => {
       console.log(error)
@@ -26,14 +33,15 @@ onMounted(() => {
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     
     <router-link
-      :to="{name: 'EventList', query: {page: page - 1}}"
+      :to="{ name: 'EventList', query: { page: page - 1 } }"
       rel="prev"
       v-if="page != 1"
       >Prev page</router-link>
 
-      <router-link
-      :to="{name: 'EventList', query: {page: page + 1}}"
+    <router-link
+      :to="{ name: 'EventList', query: { page: page + 1 } }"
       rel="next"
+      v-if="hasNextPage"
       >Next page</router-link>
   </div>
   <div class="events" v-else>
