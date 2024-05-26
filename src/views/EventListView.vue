09177a9fb78d0d7a7e0eea910cdb179bef62f148
José David Rodriguez Payo <josedavidrp12@gmail.com>
2024-05-26 21:14:48 +0200

    pagination style

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -5,12 +5,11 @@ import EventService from '@/services/EventService.js'
 const props = defineProps(['page'])
 const events = ref(null)
 const totalEvents = ref(0)
-
+const totalPages = ref(0)
 const page = computed(() => props.page)
 
 const hasNextPage = computed(() => {
-  const totalPages = Math.ceil(totalEvents.value / 2)
-  return page.value < totalPages
+  return page.value < totalPages.value
 })
 onMounted(() => {
   watchEffect(() => {
@@ -19,6 +18,7 @@ onMounted(() => {
       .then((response) => {
         events.value = response.data
         totalEvents.value = response.headers['x-total-count']
+        totalPages.value = Math.ceil(totalEvents.value / 2)
       })
       .catch((error) => {
         console.log(error)
@@ -31,20 +31,31 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <div class="events" v-if="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-
-    <router-link
-      :to="{ name: 'event-list', query: { page: page - 1 } }"
-      rel="prev"
-      v-if="page != 1"
-      >Prev page</router-link
-    >
-
-    <router-link
-      :to="{ name: 'event-list', query: { page: page + 1 } }"
-      rel="next"
-      v-if="hasNextPage"
-      >Next page</router-link
-    >
+    <div class="pagination">
+      <span style="flex: 1">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        v-show="page != 1"
+        >Prev page</router-link
+      >
+    </span>
+      <router-link
+        v-for="n in totalPages"
+        :to="{ name: 'event-list', query: { page: n } }"
+        >{{ n }}</router-link
+      >
+      <span style="flex: 1">
+      <router-link
+        id="page-next"
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        v-show="hasNextPage"
+        >Next page</router-link
+      >
+    </span>
+    </div>
   </div>
   <div class="events" v-else>
     <p>No events available or no internet connection</p>
@@ -56,5 +67,21 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
